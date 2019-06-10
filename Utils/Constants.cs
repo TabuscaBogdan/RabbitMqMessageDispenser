a895da47df3eeecaa408a56da43253caf69ec91d
Ecaterina Mihaela Chilaboc <dev.c.ecaterina.mihaela@gmail.com>
2019-06-10 22:17:35 +0300

    Generated 10000 of publications and subscriptions. Now the publications are readed over and over again for 5 minutes.

--- Utils/Constants.cs
+++ Utils/Constants.cs
@@ -5,8 +5,8 @@ namespace Utils
     public static class Constants
     {
         public static readonly String RabbitMqServerAddress = "localhost";
-        public static readonly String PublicationsFileName = "Resources/test_small_files/publications_P{0}.txt";
-        public static readonly String SubscriptionsPath = "Resources/test_small_files/subscriptions_c{0}.txt";
+        public static readonly String PublicationsFileName = "Resources/test_10000/publications_P{0}.txt";
+        public static readonly String SubscriptionsPath = "Resources/test_10000/subscriptions_C{0}.txt";
         public static readonly int NumberOfBrokers = 3;
     }
 }
