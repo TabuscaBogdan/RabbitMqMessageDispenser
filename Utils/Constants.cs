3c6c32cd09fc2e19ec92d61b8c517f9eb901e202
Victor-Andrei Filimon <victor.filimon@live.com>
2019-06-12 18:06:09 +0300

    - new 500

--- Utils/Constants.cs
+++ Utils/Constants.cs
@@ -7,10 +7,10 @@ namespace Utils
     {
         public static readonly String RabbitMqServerAddress = "localhost";
         public static readonly Uri AmqpServerAddress = new Uri("amqp://dguytdtm:zTOjLoU-cX1KcdMGKFDzD-_i3_e1Egrr@macaw.rmq.cloudamqp.com/dguytdtm");
-        public static readonly String PublicationsFileName = "Resources/test_10000/publications_P{0}.txt";
-        public static readonly String SubscriptionsPath = "Resources/test_10000/subscriptions_c{0}.txt";
+        public static readonly String PublicationsFileName = "Resources/test_500/publications_P{0}.txt";
+        public static readonly String SubscriptionsPath = "Resources/test_500/subscriptions_c{0}.txt";
         public static readonly int NumberOfBrokers = 3;
-        public static readonly bool RunLocal = true;
+        public static readonly bool RunLocal = false;
         public static readonly bool Debug = true;
 
         public static readonly String LatencyOutputFileName = "Resources/LatencyOutput/Latencies{0}.csv";
